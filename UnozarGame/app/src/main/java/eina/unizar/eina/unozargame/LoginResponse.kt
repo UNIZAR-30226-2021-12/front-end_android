75b9a0c1092493b51219b82d8d16f811b2ed8fa5
Berty980 <735976@unizar.es>
2021-04-03 11:26:10 +0200

    Revert "Version 1 android juego"
    This reverts commit 7f163f83be1aae3ebe4617b4b397053655e3db50.

--- UnozarGame/app/src/main/java/eina/unizar/eina/unozargame/LoginResponse.kt
+++
@@ -1,7 +0,0 @@
-package eina.unizar.eina.unozargame
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginResponse(@SerializedName("error")val error: Boolean,
-                         @SerializedName("error_message")val message:String,
-                         @SerializedName("user")val user: loginUser)
